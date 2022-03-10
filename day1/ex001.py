10f89ba03ae8ce8d15390f7ddefc28d1dcff79f0
Valter <valteralvees@outlook.com>
2022-03-10 09:34:00 -0300

    Initial commit

---
+++ day1/ex001.py
@@ -0,0 +1,11 @@
+print('Hello world')
+
+#unpacking - desestruturação
+nome,idade,curso,signo = 'valter',20,'ADS', 'aquário'
+
+#Sistema online de redirecionamento e chatbot com o aluno UNIT - Magister
+nome = input('Qual o seu nome?')
+curso = input('Qual o seu curso?')
+print('Oi,' + nome + "! " + "Seja bem vindo ao MAGISTER da Unit. É um prazer tê-lo conosco." + " Seu curso de " + curso + " Está disponível para consulta no sistema online. O que vocÊ deseja fazer? ")
+
+
